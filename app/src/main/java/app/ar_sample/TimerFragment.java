58187df101b9405373bc8f74ac58b880f744ca77
r_dos <r_dos00@mail.ru>
2018-04-14 22:01:24 +0300

    TimerFragment + TimerMan.it`s work TODO: mini-refactoring!

--- app/src/main/java/app/ar_sample/TimerFragment.java
+++ app/src/main/java/app/ar_sample/TimerFragment.java
@@ -1,68 +1,107 @@
 package app.ar_sample;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.TimerTask;
 
-public class TimerFragment extends Fragment implements TimerMan.Callback {
+public class TimerFragment extends Fragment implements TimerMan.Callback, View.OnClickListener, DialogInterface.OnClickListener {
+
+    private final long INIT_SECONDS = 20;
+    private final long ADD_SECONDS = 5;
 
     private TextView mTimerTextView;
+    private TextView mCountBtnPlusTextView;
+
     private TimerMan mTimerMan;
-    private MyCountDownTimer mTimer1;
+    private AlertDialog mAlertDialog;
+    private boolean mIsShowAlertDialog = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        //mTimerMan = new TimerMan(this);
-        mTimer1 = new MyCountDownTimer();
+
+        mTimerMan = new TimerMan(this, INIT_SECONDS);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
-        mTimerTextView = (TextView) view.findViewById(R.id.tv_timer);
+        mTimerTextView = (TextView) view.findViewById(R.id.text_timer);
+        mCountBtnPlusTextView = (TextView) view.findViewById(R.id.text_count_btn_plus);
+        Button btnPlusSeconds = (Button) view.findViewById(R.id.btn_plus_seconds);
+        btnPlusSeconds.setText(getString(R.string.btn_plus_seconds, ADD_SECONDS));
+        btnPlusSeconds.setOnClickListener(this);
+        setFragmentData();
         return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false)
+                .setPositiveButton(getString(R.string.btn_plus_seconds, INIT_SECONDS), this)
+                .setNegativeButton(R.string.btn_close, this);
+        mAlertDialog = builder.create();
+        if (mIsShowAlertDialog) {
+            mAlertDialog.show();
+        }
+    }
+
+    private void setFragmentData() {
+        mTimerTextView.setText(mTimerMan.getTimerStrFormat());
+        mCountBtnPlusTextView.setText(getString(R.string.text_count_btn_plus, mTimerMan.getAddSecsCount()));
     }
 
     @Override
-    public void onTimerTick(long seconds) {
-        mTimerTextView.setText("Осталось: " + seconds);
+    public void onTimerTick() {
+        if (getActivity() == null) {
+            return;
+        }
+        setFragmentData();
     }
 
     @Override
     public void onTimerFinish() {
-        mTimerTextView.setText("onFinish!");
+        if (getActivity() == null) {
+            return;
+        }
+        mAlertDialog.show();
+        mIsShowAlertDialog = true;
     }
 
-    private class MyCountDownTimer extends CountDownTimer {
-
-        public MyCountDownTimer() {
-            super(40000, 500);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_plus_seconds:
+                mTimerMan.addSeconds(ADD_SECONDS);
+                break;
         }
+    }
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-            mTimerTextView.setText("Осталось111: " + millisUntilFinished / 1000);
-        }
-
-        @Override
-        public void onFinish() {
-            mTimerTextView.setText("onFinish!1111111");
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        mIsShowAlertDialog = false;
+        switch (which)
+        {
+            case DialogInterface.BUTTON_POSITIVE:
+                mTimerMan.initSeconds(INIT_SECONDS);
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                // No button clicked
+                break;
         }
     }
 }
--- app/src/main/java/app/ar_sample/TimerMan.java
+++ app/src/main/java/app/ar_sample/TimerMan.java
@@ -2,27 +2,111 @@ package app.ar_sample;
 
 import android.os.CountDownTimer;
 
-class TimerMan extends CountDownTimer {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+
+final class TimerMan {
 
     private final Callback mCallback;
 
-    public TimerMan(Callback callback) {
-        super(30000, 333);
+    private CntDownTimer mCntDownTimer;
+    private long mCurrentSecs = 0;
+    private long mAddSecsCount = 0;
+
+    private Calendar mCalendar;
+    private SimpleDateFormat mSimpleDateFormat;
+
+    public TimerMan(Callback callback, long seconds) {
+        super();
         mCallback = callback;
+        initSeconds(seconds);
     }
 
-    @Override
-    public void onTick(long millisUntilFinished) {
-        mCallback.onTimerTick(millisUntilFinished / 1000);
+    public void initSeconds(long seconds) {
+        addSeconds(seconds, false);
     }
 
-    @Override
-    public void onFinish() {
-        mCallback.onTimerFinish();
+    public void addSeconds(long seconds) {
+        addSeconds(seconds, true);
+    }
+
+    private void addSeconds(long seconds, boolean isAdditionSeconds) {
+        if ((mCurrentSecs + seconds) * 1000 <= 0) {
+            return;
+        }
+        if (isAdditionSeconds)  {
+            mAddSecsCount += 1;
+        }
+        mCurrentSecs += seconds;
+        stopTimer();
+        startNewTimer(mCurrentSecs);
+    }
+
+    public String getTimerStrFormat() {
+        //TODO: может нужно было бы сделать тут проще? ;)
+        getCalendar().setTimeInMillis(mCurrentSecs * 1000);
+        return getSimpleDateFormat().format(getCalendar().getTime());
+    }
+
+    private Calendar getCalendar() {
+        if (mCalendar == null) {
+            mCalendar = Calendar.getInstance();
+//            mCalendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        }
+        return mCalendar;
+    }
+
+    private SimpleDateFormat getSimpleDateFormat() {
+        if (mSimpleDateFormat == null) {
+            mSimpleDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+            mSimpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        }
+        return mSimpleDateFormat;
+    }
+
+    private void startNewTimer(long seconds) {
+        mCntDownTimer = new CntDownTimer(seconds);
+        mCntDownTimer.start();
+    }
+
+    private void stopTimer() {
+        if (mCntDownTimer == null) {
+            return;
+        }
+        mCntDownTimer.cancel();
+    }
+
+    public long getAddSecsCount() {
+        return mAddSecsCount;
+    }
+
+
+    private class CntDownTimer extends CountDownTimer {
+
+        private CntDownTimer(long secs) {
+            super(secs * 1000, 1000);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            mCurrentSecs = millisUntilFinished / 1000;
+            //TODO: onTimerTick только после изменений??
+            mCallback.onTimerTick();
+        }
+
+        @Override
+        public void onFinish() {
+            mCurrentSecs = 0;
+            mCallback.onTimerTick();
+            mCallback.onTimerFinish();
+        }
     }
 
     public interface Callback {
-        void onTimerTick(long seconds);
+        void onTimerTick();
         void onTimerFinish();
     }
 }
