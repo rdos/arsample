0090250b8ad6eb19859d413e6190748c42b83b24
r_dos <r_dos00@mail.ru>
2018-04-14 16:16:47 +0300

    init commit

---
+++ app/src/main/java/app/ar_sample/TimerActivity.java
@@ -0,0 +1,13 @@
+package app.ar_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
